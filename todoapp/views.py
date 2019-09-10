b70c60ea009f1b540b1610787fa71afbad9bc1b8
ioana <ioanav3796@gmail.com>
2019-09-10 13:45:42 +0200

    adding to-dos and displaying them on index page on submit

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'GET':
+        todo_items = TodoItem.objects.all()
+        context = {
+            'todos': todo_items
+        }
+        return render(request, 'todoapp/index.html', context)
+
+    if request.method == "POST":
+        todo_item = TodoItem()
+        todo_item.title = request.POST['title']
+        todo_item.status = False
+        todo_item.save()
+        return HttpResponseRedirect(reverse('todoapp:index'))
